bc4dfdfc03ae8bda2205107c9fdef78173a9f1d5
Veijo Väisänen <vvaisan@oamk.fi>
2018-01-08 16:54:43 +0200

    Added changeAmount method

--- MyApp.php
+++ MyApp.php
@@ -14,20 +14,24 @@ class Product
         $this->amount = $amount;
     }
 
-    public
-    function countValue()
+    public function countValue()
     {
         return ($this->amount * $this->price);
     }
 
-    public
-    function printProduct()
+    public function printProduct()
     {
         printf("Product %s, price %4.1f and balance %d pcs", $this->name,$this->price,$this->amount);
+    }
+	
+	 public function changeAmount($amount)
+    {
+        $this->amount = $amount;
     }
 }
 
 $product = new Product("Edam", 3.3, 120);
+$product->changeAmount(200);
 printf("Product value is %4.1f", $product->countValue());
 echo "<br>";
 $product->printProduct();
