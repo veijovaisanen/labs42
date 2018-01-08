79ca345b54d6cb3353558675ba8a85512673654b
Veijo Väisänen <vvaisan@oamk.fi>
2018-01-08 17:00:38 +0200

    Added changePrice method

--- MyApp.php
+++ MyApp.php
@@ -25,9 +25,15 @@ class Product
     {
         printf("Product %s, price %4.1f and balance %d pcs", $this->name,$this->price,$this->amount);
     }
+	
+	 public function changePrice($price)
+    {
+        $this->price = $price;
+    }
 }
 
 $product = new Product("Edam", 3.3, 120);
+$product->changePrice(3.8);
 printf("Product value is %4.1f", $product->countValue());
 echo "<br>";
 $product->printProduct();
